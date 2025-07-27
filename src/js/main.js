265b7185be278c9922e561e1f9d6df08c8d1a908
Alexander Prado <alexanderprado63@gmail.com>
2025-07-27 09:08:44 -0400

    Testing

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,30 @@
+fetch('./src/data/courses.json')
+    .then(res => res.json())
+    .then(courses => {
+        return fetch('./src/data/teachers.json')
+            .then(res => res.json())
+            .then(teachers => {
+                const cursosCompletos = courses.map(curso => {
+                    const teacher = teachers.find(t => t.id === curso.teacherId);
+                    return {
+                        ...curso,
+                        teacher: {
+                            nombre: teacher.nombre,
+                            imgPerfil: teacher.imgPerfil
+                        }
+                    };
+                });
+
+                const contenedor = document.querySelector('.course-grid');
+                cursosCompletos.forEach(course => {
+                    const cardHTML = crearCardHTML(course);
+                    contenedor.innerHTML += cardHTML;
+                });
+                contenedor.innerHTML = cursosCompletos.map(crearCardHTML).join('');
+            });
+    })
+    .catch(error => console.error("Error cargando datos:", error));
+
 function crearCardHTML(course) {
     return `
     <div class="course-card">
@@ -31,30 +58,3 @@ function crearCardHTML(course) {
     </div>
   `;
 }
-
-fetch('./src/data/courses.json')
-    .then(res => res.json())
-    .then(courses => {
-        return fetch('./src/data/teachers.json')
-            .then(res => res.json())
-            .then(teachers => {
-                const cursosCompletos = courses.map(curso => {
-                    const teacher = teachers.find(t => t.id === curso.teacherId);
-                    return {
-                        ...curso,
-                        teacher: {
-                            nombre: teacher.nombre,
-                            imgPerfil: teacher.imgPerfil
-                        }
-                    };
-                });
-
-                const contenedor = document.querySelector('.course-grid');
-                cursosCompletos.forEach(course => {
-                    const cardHTML = crearCardHTML(course);
-                    contenedor.innerHTML += cardHTML;
-                });
-                contenedor.innerHTML = cursosCompletos.map(crearCardHTML).join('');
-            });
-    })
-    .catch(err => console.error("Error cargando datos:", err));
